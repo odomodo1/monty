23ceabe4ac1505ecb65510f56c8c50a40c88bc59
Provoski <provo.iyen27@gmail.com>
2023-03-23 14:22:38 -0700

    formatting header file

--- monty.h
+++ monty.h
@@ -2,11 +2,11 @@
 #define _MONTY_
 
 /* Constants */
-#define SUCSS_OP		0
-#define VALID_PARM		0
-#define MIN_ARGS		2
-#define METH_STACK		300
-#define METH_QUEUE		301
+#define SUCSS_OP	0
+#define VALID_PARM	0
+#define MIN_ARGS	2
+#define METH_STACK	300
+#define METH_QUEUE	301
 
 /* Common Errors */
 #define ERR_BAD_INST	100
@@ -14,19 +14,19 @@
 #define ERR_INVLD_PARM	102
 
 /* Usage Errors */
-#define ERR_ARG_USG		200
+#define ERR_ARG_USG	200
 #define ERR_PUSH_USG	201
 #define ERR_PINT_USG	202
-#define ERR_POP_USG		203
+#define ERR_POP_USG	203
 #define ERR_SWAP_USG	204
-#define ERR_ADD_USG		205
-#define ERR_SUB_USG		206
-#define ERR_DIV_USG		207
-#define ERR_DIV_ZRO		208
-#define ERR_MUL_USG		209
-#define ERR_MOD_USG		210
-#define ERR_PCH_USG		211
-#define ERR_PCH_EMP		212
+#define ERR_ADD_USG	205
+#define ERR_SUB_USG	206
+#define ERR_DIV_USG	207
+#define ERR_DIV_ZRO	208
+#define ERR_MUL_USG	209
+#define ERR_MOD_USG	210
+#define ERR_PCH_USG	211
+#define ERR_PCH_EMP	212
 
 #include <ctype.h>
 #include <fcntl.h>
